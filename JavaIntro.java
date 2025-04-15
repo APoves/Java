101777882daec1fc2478c2b77584790e3492ee24
Aurora Marina Poves <164747570+APoves@users.noreply.github.com>
2025-04-15 12:56:56 +0200

    Add files via upload

---
+++ JavaIntro.java
@@ -0,0 +1,13 @@
+package BasicoJava;
+
+
+public class JavaIntro {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo!");
+	}
+
+}
+	// Escribe un Hola Mundo y muéstralo.
+	 
