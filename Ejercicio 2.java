386f94cf6680d10d61686bc1d2b3d9442aa834c9
Aurora Marina Poves <164747570+APoves@users.noreply.github.com>
2025-04-22 19:54:32 +0200

    Update Ejercicio 2.java

--- Ejercicio 2.java
+++ Ejercicio 2.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 public class JavaIntro {
 
 	public static void main(String[] args) {
-		
+		//Iniciar Scanner
 		Scanner sn = new Scanner (System.in);
-		sn.useDelimiter("/n/r");
-		
+
+		//Pedir datos
 		System.out.println("Introduce tu nombre");
 		String nombre = sn.nextLine();
-		
+
+		//Mostrar datos
 		System.out.println("Hola " + nombre);
-		
+
+		//Cerrar Scanner
 		sn.close();
 	}
 
