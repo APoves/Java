9afbf7eaabd3e11630211aeecd7bc1c2ccb79cb2
Aurora Marina Poves <164747570+APoves@users.noreply.github.com>
2025-04-23 18:55:03 +0200

    Update JavaIntro.java

--- JavaIntro.java
+++ JavaIntro.java
@@ -1,5 +1,4 @@
 package BasicoJava;
-
 import java.util.Scanner;
 
 public class JavaIntro {
@@ -9,12 +8,13 @@ public class JavaIntro {
 		//Iniciar Scanner.
 		Scanner sn = new Scanner (System.in);
 				
+		//Declarar variable resultado.
 		int result;
 
 		//Pedir número;
 		System.out.println("Introduce un número para mostrar su tabla de multiplicar: ");
 		int num = sn.nextInt();
-		
+
 		System.out.println("La tabla de multiplicar de " + num + " es: ");
 		
 		//Bucle for (controla los números del 1 al 10 y muestra la tabla de multiplicar del
@@ -22,16 +22,13 @@ public class JavaIntro {
 		for (int i = 1; i <= 10; i++) {
 			result = (num * i);
 			System.out.println (num + " * " + i + " = " + result + ".");
-			
 		}
 
 		//Cerrar Scanner.
 			sn.close();
-
 	}
 }
 
 	/*EJERCICIO 9:
 	 * Pide un número por teclado y muestra su tabla de multiplicar hasta 10.
-	 * (Pedir número y recorrer del 1 al 10).
 	 */ 
