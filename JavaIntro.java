6312225f81c87c4bbd4d65d02d3461b06e665283
Aurora Marina Poves <164747570+APoves@users.noreply.github.com>
2025-04-23 18:40:16 +0200

    Update JavaIntro.java

--- JavaIntro.java
+++ JavaIntro.java
@@ -6,7 +6,7 @@ public class JavaIntro {
 		
 		System.out.print("Los números del 1 al 100 divisibles entre 2 y 3, son: ");
 		//Bucle for (con declaración de variable y condición hasta salir del bucle).
-		for (int i = 1; i <= 100; i++){
+		for (int i = 1; i <= 100; i++){ //Al ser un rando definido (de 1 a 100) se utiliza for.
 			if (i % 2 == 0 && i % 3 == 0){
 				//Mostrar números.
 				System.out.print(i + " ");
@@ -15,8 +15,5 @@ public class JavaIntro {
 
 	}
 }
-
-	/* EJERCICIO 8:
-	 * Muestra los números del 1 y al 100 que sean divisibles entre 2 y 3.
-	 * (Como el rango está definido, se utilizará FOR).
-	 */
+	 // Muestra los números del 1 y al 100 que sean divisibles entre 2 y 3.
+	 
